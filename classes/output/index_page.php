22d31cd819b47f6e49d450832422430a4a22fc9f
Bruno Malaval <bruno.malaval@uha.fr>
2019-04-11 11:31:44 +0200

    modifié :         classes/output/index_page.php

--- classes/output/index_page.php
+++ classes/output/index_page.php
@@ -1,5 +1,21 @@
 <?php
-// Standard GPL and phpdocs
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+defined('MOODLE_INTERNAL') || die();
+
 namespace report_cohortdetail\output;
 
 use renderable;
@@ -9,9 +25,9 @@ use stdClass;
 
 class index_page implements renderable, templatable {
 
-    /** @var string $sometext Some text to show how to pass data to a template. */
+    //@var string $sometext Some text to show how to pass data to a template.
 
-    var $sometext = null;
+    $sometext = null;
 
     public function __construct($sometext) {
 
